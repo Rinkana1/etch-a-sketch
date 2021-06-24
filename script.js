e6f1d95238942f1a7af58cadb298e91a713d9b05
Rinkana1 <mcjuno83@gmail.com>
2021-06-24 16:31:01 -0400

    Convert to jQuery and add grid items

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const container = document.getElementById('js-container')
-for(var i = 0; i < 16; i++) {
+const container = $('#js-container');
+const pixel = $('.pixel')
 
+function createGrid(side) {
+    square = side * side;
+    var divList = '';
+    for(var i = 0;i < square; i++) {
+        divList += '<div class="pixel" id="js-pixel"></div>';
+    }
+    container.html(divList);
+    var columns;
+    var pixelHeight = 95 / side;
+    var pixelWidth = 100 / side;
+
+    pixel.css({'height':`${pixelHeight}vh`,'width':`${pixelWidth}vw`})
+    
+    for(var j = 0; j < side; j++) {
+        columns += "auto ";
+    }
+    
 }
+
+createGrid(16);
