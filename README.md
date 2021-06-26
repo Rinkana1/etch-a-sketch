bc1912d2806348b572b4da455c25fde1864fecec
Rinkana1 <mcjuno83@gmail.com>
2021-06-26 01:30:13 -0400

    Complete hover event, clearGrid function, and Update README.md

--- README.md
+++ README.md
@@ -3,13 +3,13 @@ In this project, I will create a makeshift sketchpad in a browser using DOM mani
 
 ## Tasks
 * ~~Create a 16x16 grid of square divs~~
-* Create a hover event that changes the color of each div when the mouse hovers over it
-* Add a button to the top of the screen
-    * Clears current grid
-    * Sends user a popup asking for how many squares per side to make the new grid
-    * Grids should be generated in the same total space as before
-    * Limit for user input should be a maximum of 100
-* Try to make color variation from white to black and make each square darker as the mous passes through it more.
+* ~~Create a hover event that changes the color of each div when the mouse hovers over it~~
+* ~~Add a button to the top of the screen~~
+    * ~~Clears current grid~~
+    * ~~Sends user a popup asking for how many squares per side to make the new grid~~
+    * ~~Grids should be generated in the same total space as before~~
+    * ~~Limit for user input should be a maximum of 100~~
+* ~~Try to make color variation from white to black and make each square darker as the mous passes through it more.~~
 
 ### Some Initial Ideas to Accomplish These Tasks
 * Keep the whole system in a large div
@@ -18,3 +18,6 @@ In this project, I will create a makeshift sketchpad in a browser using DOM mani
     * When the mouse hovers over a square, a counter will tick up moving to the next color in the array.
 * Use viewport units to make sure each square is within the constraints of the window
     * Divide 100 by the number of squares to get an exact number
+
+## Thoughts at the End
+Personally for me, this was pretty easy since I already have a grasp on using jQuery. I didn't do the optional challenge of making every pixel be a random rgb value, but I think I effectively finished the project. I did learn how to automate making HTML DOM elements, and I have a much better grasp on CSS grid layout, so I think I still got some things out of this. I could've done this all using JavaScript and no jQuery, but to be honest, I got lazy and decided that jQuery was faster and easier.
--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const container = $('#js-container');
 const pixel = $('.pixel')
-const colors = ['#ffffff', '#dddddd', '#bbbbbb', '#999999', '#777777', '#555555', '#333333', '#000000']
+const reset = $('#js-reset')
+const colors = ['rgb(255, 255, 255)', 'rgb(221, 221, 221)', 'rgb(187, 187, 187)', 'rgb(153, 153, 153)', 'rgb(119, 119, 119)', 'rgb(85, 85, 85)', 'rgb(51, 51, 51)', 'rgb(0, 0, 0)']
 
 function createGrid(side) {
     square = side * side;
     var divList = '';
     for(var i = 0;i < square; i++) {
-        divList += '<div class="pixel" id="js-pixel"></div>';
+        divList += '<div class="pixel color_0" id="js-pixel" style="background-color: #ffffff;"></div>';
     }
     container.html(divList);
     var columns = '';
@@ -14,11 +15,68 @@ function createGrid(side) {
     for (var j = 0; j < side; j++) {
         columns += 'auto ';
     }
-    $('.container').css('grid-template-columns', columns)
+    $('.container').css('grid-template-columns', columns);
 }
 
-function color() {
-    
+
+
+
+function color(item) {
+    switch ($(item).css('background-color')) {
+        case colors[0]:
+            $(item).css('background-color', colors[1]);
+            break;
+        case colors[1]:
+            $(item).css('background-color', colors[2]);
+            break;
+        case colors[2]:
+            $(item).css('background-color', colors[3]);
+            break;
+        case colors[3]:
+            $(item).css('background-color', colors[4]);
+            break;
+        case colors[4]:
+            $(item).css('background-color', colors[5]);
+            break;
+        case colors[5]:
+            $(item).css('background-color', colors[6]);
+            break;
+        case colors[6]:
+            $(item).css('background-color', colors[7]);
+            break;
+    }
 }
+    
+function clearGrid() {
+    container.empty();
+
+    let newGrid = prompt('How many pixels do you want on each side?')
+
+    if(isNaN(newGrid)) {
+        alert('This value must be a number!')
+        console.error('NaN. GENERATING 16x16 GRID.')
+        createGrid(16);
+    } else if(Number.isInteger(newGrid)) {
+        alert('You can\'t use decimals or fractions!')
+        console.error('MUST BE INTEGER. GENERATING 16x16 GRID.')
+    } else if(newGrid > 100) {
+        alert('You can\'t have more than 100 pixels on each side!')
+        console.error('TOO MANY PIXELS. GENERATING 16x16 GRID.')
+        createGrid(16);
+    } else if(newGrid < 0) {
+        alert('This value is not valid!')
+        console.error('CANNOT BE NEGATIVE. GENERATING 16x16 GRID.')
+        createGrid(16);
+    } else {
+        createGrid(newGrid)
+    }
+
+    $('.pixel').mouseenter(function() {color(this)});
+}
+
+
 
 createGrid(16);
+
+$('.pixel').mouseenter(function() {color(this)});
+reset.click(clearGrid)
